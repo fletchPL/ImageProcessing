7e91c563c50537de7cb82d4a17020d5a14bc97e1
fletchPL <wisniewski.maciek1@gmail.com>
2017-03-16 19:36:07 +0100

    Change Label Name

--- ImageProcessing/Form1.Designer.cs
+++ ImageProcessing/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(547, 469);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SMPD";
             this.ResumeLayout(false);
 
         }
