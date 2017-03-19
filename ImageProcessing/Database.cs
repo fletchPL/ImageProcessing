c87569feebc27d7fc8dcbecb1f5f659fcce4c9a7
fletchPL <wisniewski.maciek1@gmail.com>
2017-03-19 18:49:05 +0100

    Add default constructor

--- ImageProcessing/Database.cs
+++ ImageProcessing/Database.cs
@@ -8,9 +8,24 @@ namespace ImageProcessing
 {
     class Database
     {
-        private List<Object> objects = new List<Object>();                              //Lista w C# zamiast Vectora w C++
-        private Dictionary<string,int> classCounters = new Dictionary<string, int>();   //Dictionary w C# zamiast map w C++
-        private List<String> classNameVector = new List<string>();                      //Lista w C# zamiast Vectora w C++
-        private List<uint> featuresIDs = new List<uint>();                                //Lista w C# zamiast Vectora w C++
+        #region Private Variables
+            private List<Object> objects = new List<Object>();                              //Lista w C# zamiast Vectora w C++
+            private Dictionary<string,int> classCounters = new Dictionary<string, int>();   //Dictionary w C# zamiast map w C++
+            private List<String> classNameVector = new List<string>();                      //Lista w C# zamiast Vectora w C++
+            private List<uint> featuresIDs = new List<uint>();                              //Lista w C# zamiast Vectora w C++
+
+            private uint noClass;
+            private uint noObjects;
+            private uint noFeatures;
+        #endregion
+
+        #region Public Variables
+            public Database ()
+            {
+                noClass = 0;
+                noObjects = 0;
+                noFeatures = 0;
+            }
+        #endregion
     }
 }
