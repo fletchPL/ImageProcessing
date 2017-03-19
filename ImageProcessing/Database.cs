624a4c65b0415de28b8252541bfb678ad3f7a7b1
fletchPL <wisniewski.maciek1@gmail.com>
2017-03-19 18:40:59 +0100

    Start

--- ImageProcessing/Database.cs
+++ ImageProcessing/Database.cs
@@ -11,5 +11,6 @@ namespace ImageProcessing
         private List<Object> objects = new List<Object>();                              //Lista w C# zamiast Vectora w C++
         private Dictionary<string,int> classCounters = new Dictionary<string, int>();   //Dictionary w C# zamiast map w C++
         private List<String> classNameVector = new List<string>();                      //Lista w C# zamiast Vectora w C++
+        private List<int> featuresIDs = new List<int>();                                //Lista w C# zamiast Vectora w C++
     }
 }
