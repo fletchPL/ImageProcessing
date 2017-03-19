de3e5fd80803946f8bcaa16e320e2f0fd0cdf65d
fletchPL <wisniewski.maciek1@gmail.com>
2017-03-19 18:42:54 +0100

    update in variables

--- ImageProcessing/Database.cs
+++ ImageProcessing/Database.cs
@@ -11,6 +11,6 @@ namespace ImageProcessing
         private List<Object> objects = new List<Object>();                              //Lista w C# zamiast Vectora w C++
         private Dictionary<string,int> classCounters = new Dictionary<string, int>();   //Dictionary w C# zamiast map w C++
         private List<String> classNameVector = new List<string>();                      //Lista w C# zamiast Vectora w C++
-        private List<int> featuresIDs = new List<int>();                                //Lista w C# zamiast Vectora w C++
+        private List<uint> featuresIDs = new List<uint>();                                //Lista w C# zamiast Vectora w C++
     }
 }
