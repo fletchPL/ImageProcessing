a4737dd4526a93bdad5e4db4375fd2baf6ad1016
fletchPL <wisniewski.maciek1@gmail.com>
2017-03-19 18:34:03 +0100

    Start to copywrite a program from C++ to C#

--- ImageProcessing/Database.cs
+++ ImageProcessing/Database.cs
@@ -8,8 +8,8 @@ namespace ImageProcessing
 {
     class Database
     {
-        private List<Object> objects = new List<Object>(); // Lista w C# zamiast Vectora w C++
-        private Dictionary<string,int> map = new Dictionary<string, int>(); //Dictionary w C# zamiast map w C++
-
+        private List<Object> objects = new List<Object>();                              //Lista w C# zamiast Vectora w C++
+        private Dictionary<string,int> classCounters = new Dictionary<string, int>();   //Dictionary w C# zamiast map w C++
+        private List<String> classNameVector = new List<string>();                      //Lista w C# zamiast Vectora w C++
     }
 }
