54ecf9d47e0416c8ad45f3a00568913ad3d3ac9f
fletchPL <wisniewski.maciek1@gmail.com>
2017-03-24 14:10:16 +0100

    Implement load function

--- ImageProcessing/Database.cs
+++ ImageProcessing/Database.cs
@@ -11,9 +11,9 @@ namespace ImageProcessing
     {
         #region Private Variables
         private List<Object> objects = new List<Object>();                              //Lista w C# zamiast Vectora w C++
-        private Dictionary<string, int> classCounters = new Dictionary<string, int>();   //Dictionary w C# zamiast map w C++
+        private Dictionary<string, int> classCounters = new Dictionary<string, int>();  //Dictionary w C# zamiast map w C++
         private List<String> classNameVector = new List<string>();                      //Lista w C# zamiast Vectora w C++
-        private List<uint> featuresIDs = new List<uint>();                              //Lista w C# zamiast Vectora w C++
+        private List<float> featuresIDs = new List<float>();                              //Lista w C# zamiast Vectora w C++
 
         private uint noClass;
         private uint noObjects;
@@ -21,6 +21,8 @@ namespace ImageProcessing
         #endregion
 
         #region Public function 
+
+        public string[] strArry;
         public Database()
         {
             noClass = 0;
@@ -53,18 +55,91 @@ namespace ImageProcessing
         public bool load(string fileName) // FUNKCJA LOAD JEST CAŁA DO POPRAWY
         {
             clear();
-            string[] strArry;
+            
            try
             {
                 using (StreamReader sr = new StreamReader(fileName))
                 {
-                    string document = sr.ReadToEnd();
-                    int size = document.Length;
+                    string line = sr.ReadLine();
+                    int positionOfFirstSign = line.IndexOf(',');
 
-                    for(int i=0;i<size;i++)
+                    if (positionOfFirstSign.Equals(null))
                     {
-                        strArry = document.Split(',');
+                        return false;
                     }
+
+                    uint classFeaturesNo = uint.Parse(line.Substring(0, positionOfFirstSign));
+
+                    string featuresID = line.Substring(positionOfFirstSign + 1);
+                    
+                    while(true)
+                    {
+                        positionOfFirstSign = featuresID.IndexOf(',');
+
+                        if(positionOfFirstSign != null)
+                        {
+                            string feature = featuresID.Substring(0, positionOfFirstSign);
+                            featuresID = featuresID.Substring(positionOfFirstSign + 1);
+                            float featureID = float.Parse(feature);
+                            featuresIDs.Add(featureID);
+                        }
+                        else
+                        {
+                            float featureID = float.Parse(featuresID);
+                            featuresIDs.Add(featureID);
+                            break;
+                        }
+                    }
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        int pos = line.IndexOf(',');
+
+                        if (pos.Equals(null))
+                        {
+                            continue;
+                            //throw new Exception("Index not found");
+                        }
+
+                        string className = line.Substring(0, pos);
+                        int classNamePos = className.IndexOf(' ');
+
+                        if (classNamePos != null)
+                        {
+                            className = className.Substring(0, classNamePos);
+                        }
+
+                        string features = line.Substring(pos + 1);
+                        List<float> featuresValues = new List<float>();
+
+                        while(true)
+                        {
+                            pos = features.IndexOf(',');
+                            if(pos != null)
+                            {
+                                string feature = features.Substring(0, pos);
+                                features = features.Substring(pos + 1);
+                                float featuresValue = float.Parse(feature);
+                                featuresValues.Add(featuresValue);
+                            }
+                            else
+                            {
+                                float featureValue = float.Parse(features);
+                                featuresValues.Add(featureValue);
+                                break;
+                            }
+                        }
+
+                        if (classFeaturesNo == featuresValues.Count())
+                        {
+                           /* if (addObject(new Object(className, featuresValues)))
+                            {
+
+                            }*/
+                        }
+                        else return false; 
+
+                    }
+                   
                 }
             }catch(Exception e)
             {
